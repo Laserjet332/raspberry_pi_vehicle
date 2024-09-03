c7df5cc8de6ee5de8cc984abf9344489c945941c
Laserjet332 <pawellato03@gmail.com>
2024-09-03 13:35:13 +0200

    Added program to test motors

--- python/main.py
+++ python/main.py
@@ -1,3 +1,25 @@
 from ui import Interface
+from devices import Motor
+
+OUTPUT1 = 
+OUTPUT2 = 
+ENABLE1 = 
+OUTPUT3 = 
+OUTPUT4 = 
+ENABLE2 = 
+
+def update_value_motor1(event):
+    motor1.change_speed(interface.slider1.get())
+
+def update_value_motor2(event):
+    motor2.change_speed(interface.slider2.get())
+    
 
 interface = Interface()
+interface.slider1.bind("<ButtonRelease-1>",update_value_motor1)
+interface.slider2.bind("<ButtonRelease-1>",update_value_motor2)
+
+motor1 = Motor(OUTPUT1,OUTPUT2,ENABLE1)
+motor2 = Motor(OUTPUT3,OUTPUT4,ENABLE2)
+
+interface.window.mainloop()
