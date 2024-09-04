01804e3b1cc14cb2af23958c4173e2775e0d24a6
Laserjet332 <pawellato03@gmail.com>
2024-09-04 13:23:01 +0200

    Updated robot testing program

--- python/main.py
+++ python/main.py
@@ -1,27 +1,90 @@
 from ui import Interface
-from devices import Motor
+from devices import Robot
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 OUTPUT1 = 14  
 OUTPUT2 = 15
 ENABLE1 = 18
-OUTPUT3 = 2
-OUTPUT4 = 3
+OUTPUT3 = 3
+OUTPUT4 = 2
 ENABLE2 = 4
+LOW_SPEED = 40
+HIGH_SPEED = 80
 
-def update_value_motor1(event):
-    motor1.change_speed(interface.slider1.get())
-
-def update_value_motor2(event):
-    motor2.change_speed(interface.slider2.get())
-    
 
 interface = Interface()
-interface.slider1.bind("<ButtonRelease-1>",update_value_motor1)
-interface.slider2.bind("<ButtonRelease-1>",update_value_motor2)
+robot = Robot(OUTPUT1,OUTPUT2,ENABLE1,OUTPUT3,OUTPUT4,ENABLE2)
 
-motor1 = Motor(OUTPUT1,OUTPUT2,ENABLE1,0)
-motor2 = Motor(OUTPUT3,OUTPUT4,ENABLE2,0)
+def go_forward(event):
+    if not interface.up_pressed:
+        interface.up_pressed = True
+        interface.up.config(relief="sunken")
+        robot.forward()
+        
+def stop_forward(event):
+    interface.up_pressed = False
+    interface.up.config(relief="raised")
+    robot.stop()
+
+interface.window.bind("<KeyPress-w>",go_forward)
+interface.window.bind("<KeyRelease-w>",stop_forward)
+
+
+def go_backward(event):
+    if not interface.down_pressed:
+        interface.down_pressed = True
+        interface.down.config(relief="sunken")
+        robot.backward()
+
+def stop_backward(event):
+    interface.down_pressed = False
+    interface.down.config(relief="raised")
+    robot.stop()
+
+interface.window.bind("<KeyPress-s>",go_backward)
+interface.window.bind("<KeyRelease-s>",stop_backward)
+
+
+def go_right(event):
+    if not interface.right_pressed:
+        interface.right_pressed = True
+        interface.right.config(relief="sunken")
+        robot.right()
+
+def stop_right(event):
+    interface.right_pressed = False
+    interface.right.config(relief="raised")
+    robot.stop_right()
+
+interface.window.bind("<KeyPress-d>",go_right)
+interface.window.bind("<KeyRelease-d>",stop_right)
+
+
+def go_left(event):
+    if not interface.left_pressed:
+        interface.left_pressed = True
+        interface.left.config(relief="sunken")
+        robot.left()
+
+def stop_left(event):
+    interface.left_pressed = False
+    interface.left.config(relief="raised")
+    robot.stop_left()
+
+interface.window.bind("<KeyPress-a>",go_left)
+interface.window.bind("<KeyRelease-a>",stop_left)
+
+
+def update_scale_label(value):
+        interface.speed_value = int(value)
+        if interface.speed_value == 1:
+            interface.speed_scale.config(label="Low Speed")
+            robot.change_speed(LOW_SPEED)
+        elif interface.speed_value == 2:
+            interface.speed_scale.config(label="High Speed")
+            robot.change_speed(HIGH_SPEED)
+            
+interface.speed_scale.config(command=update_scale_label)
 
 interface.window.mainloop()
--- python/ui.py
+++ python/ui.py
@@ -1,41 +1,41 @@
 import tkinter as tk
 from tkinter import ttk
 
+FONT = ("Ariel",20,"normal")
 
 class Interface:
     
     def __init__(self):
         self.window = tk.Tk()
-        self.window.title('Motors Control')
+        self.window.title('Robot')
         
-        self.label1 = tk.Label(self.window,text="Motor 1")
-        self.label1.grid(row=0,column=0,padx=10,pady=10) 
+        self.up = tk.Button(self.window,text="↑",font=FONT,width=2)
+        self.up.grid(row=0,column=1,padx=2,pady=2)
+        self.up_pressed = False
         
-        self.value1 = tk.IntVar()
-        self.slider1 = tk.Scale(self.window,
-                                from_=100,
-                                to=0,
-                                orient="vertical",
-                                variable=self.value1)
-        self.slider1.grid(row=1,column=0,padx=50,pady=20)
+        self.down = tk.Button(self.window,text="↓",font=FONT,width=2)
+        self.down.grid(row=1,column=1,padx=2,pady=2)
+        self.down_pressed = False
         
-        self.value2 = tk.IntVar()
-        self.slider2 = tk.Scale(self.window,
-                                from_=100,
-                                to=0,
-                                orient="vertical",
-                                variable=self.value2)
-        self.slider2.grid(row=1,column=1,padx=50,pady=20)
+        self.left = tk.Button(self.window,text="←",font=FONT,width=2)
+        self.left.grid(row=1,column=0,padx=2,pady=2)
+        self.left_pressed = False
         
-        self.label2 = tk.Label(self.window,text="Motor 2")
-        self.label2.grid(row=0,column=1,padx=10,pady=10) 
+        self.right = tk.Button(self.window,text="→",font=FONT,width=2)
+        self.right.grid(row=1,column=2,padx=2,pady=2)
+        self.right_pressed = False
+        
+        self.speed_scale = tk.Scale(self.window,from_=2,to=1,showvalue=0,label="Low Speed")
+        self.speed_value = 1
+        self.speed_scale .grid(row=0,column=4,rowspan=2,padx=30,pady=2)
         
         self.window.grid_columnconfigure((0, 1), weight=1)
         self.window.resizable(False,False)
     
-    def get_slider1_value(self):
-        return self.value_1
-    
-    def get_slider2_value(self):
-        return self.value_2
+    def update_scale_label(self,value):
+        self.speed_value = int(value)
+        if self.speed_value == 1:
+            self.speed_scale.config(label="Low Speed")
+        elif self.speed_value == 2:
+            self.speed_scale.config(label="High Speed")
         
