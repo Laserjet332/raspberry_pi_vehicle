8717ea76a35b61fcabf938081a97963b21a7285a
Laserjet332 <pawellato03@gmail.com>
2024-10-03 00:01:27 +0200

    Update files structure

--- python/gui.py
+++ python/gui.py
@@ -1,6 +1,6 @@
-from ui import Interface
-from devices import Robot
+import tkinter as tk
 import RPi.GPIO as GPIO
+from devices import Robot
 
 GPIO.setmode(GPIO.BCM)
 OUTPUT1 = 14  
@@ -9,7 +9,32 @@ ENABLE1 = 18
 OUTPUT3 = 3
 OUTPUT4 = 2
 ENABLE2 = 4
+FONT = ("Ariel",20,"normal")
 
+class Interface:
+    
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title('Robot')
+        
+        self.up = tk.Button(self.window,text="↑",font=FONT,width=2)
+        self.up.grid(row=0,column=1,padx=2,pady=2)
+        self.up_pressed = False
+        
+        self.down = tk.Button(self.window,text="↓",font=FONT,width=2)
+        self.down.grid(row=1,column=1,padx=2,pady=2)
+        self.down_pressed = False
+        
+        self.left = tk.Button(self.window,text="←",font=FONT,width=2)
+        self.left.grid(row=1,column=0,padx=2,pady=2)
+        self.left_pressed = False
+        
+        self.right = tk.Button(self.window,text="→",font=FONT,width=2)
+        self.right.grid(row=1,column=2,padx=2,pady=2)
+        self.right_pressed = False
+        
+        self.window.grid_columnconfigure((0, 1), weight=1)
+        self.window.resizable(False,False)
 
 interface = Interface()
 robot = Robot(OUTPUT1,OUTPUT2,ENABLE1,OUTPUT3,OUTPUT4,ENABLE2)
